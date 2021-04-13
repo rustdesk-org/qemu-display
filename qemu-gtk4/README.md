1906db2a3cbb48301067e03c2db027387680cacc
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-04-13 16:43:25 +0400

    Remove gtk4

--- qemu-gtk4/README.md
+++
@@ -1,5 +0,0 @@
-# QEMU Gtk4
-
-## Credits
-
-Based on [GTK Rust template](https://gitlab.gnome.org/bilelmoussaoui/gtk-rust-template)
--- qemu-gtk4/src/error.rs
+++
@@ -1,8 +0,0 @@
-use gtk::glib;
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, glib::GErrorDomain)]
-#[gerror_domain(name = "QemuGtk")]
-pub enum QemuGtkError {
-    GL,
-    Failed,
-}
